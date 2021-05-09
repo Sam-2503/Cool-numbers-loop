382be1d7dc137a80779f5834bfaf2855fed90f68
Sampraad <74760573+Sam-2503@users.noreply.github.com>
2021-05-09 14:01:13 +0530

    Create numbers.cs

---
+++ numbers.cs
@@ -0,0 +1,19 @@
+//learnt from Brackeys video :)
+using System;
+
+class numbers
+{
+  static void Main()
+  {
+    Console.Write("How many lines of numbers do you want?");
+    int lines = Convert.ToInt32(Console.ReadLine());
+    
+    for(int i = 0; i < lines; i++)
+    {
+      Console.WriteLine(Math.Pow(2, i);
+    }
+    
+  }
+  
+}
+    
